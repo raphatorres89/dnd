9b04a154f006002e462daf20e5534b55f47a90a4
Raphael Torres <rapha.oo.torres@gmail.com>
2025-11-07 21:17:30 -0300

    Ajuste no menu das Quests, layout e adição de quests faltantes

--- src/main/java/com/raphaowl/dnd/dtos/Quest.java
+++ src/main/java/com/raphaowl/dnd/dtos/Quest.java
@@ -14,16 +14,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public class Quest {
     private QuestEnum id;
-    private String title;
-    private String description;
     private QuestDifficulty difficulty;
     private QuestType type;
-    private String location;
-    private String hook;
-    private String objective;
-    private String complication;
     private List<Item> reward;
-    private String environment;
     private List<MonsterNameEnum> suggestedMonsters;
     private List<String> factionsInvolved;
     private List<String> keyNPCs;
--- src/main/java/com/raphaowl/dnd/service/QuestService.java
+++ src/main/java/com/raphaowl/dnd/service/QuestService.java
@@ -1,14 +1,20 @@
 package com.raphaowl.dnd.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Stream;
 
+import com.raphaowl.dnd.dtos.Item;
 import com.raphaowl.dnd.dtos.Quest;
 import com.raphaowl.dnd.enums.GearEnum;
 import com.raphaowl.dnd.enums.MonsterNameEnum;
-import com.raphaowl.dnd.enums.QuestDifficulty;
 import com.raphaowl.dnd.enums.QuestEnum;
-import com.raphaowl.dnd.enums.QuestType;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
@@ -17,41 +23,63 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class QuestService {
 
+    private final MessageSource messageSource;
+
     public Quest getQuest() {
-        return new Quest(
-                QuestEnum.CLEAR_GOBLIN_CAMP,
-                "Sussurros nas Minas Antigas",
-                "Mineiros desapareceram, sussurros ecoam nos túneis...",
-                QuestDifficulty.MEDIUM,
-                QuestType.RESCUE,
-                "Minas Ruinosas",
-                "Mineiros relatam sussurros vindos do subsolo.",
-                "Investigar os túneis e descobrir a origem.",
-                "A criatura é manipulada por um culto local.",
-                List.of(GearEnum.GOLD.toItem(50)),
-                "Subterrâneo",
-                List.of(MonsterNameEnum.DARKMANTLE, MonsterNameEnum.GRIMLOCK, MonsterNameEnum.CULT_FANATIC),
-                List.of("Guilda dos Mineradores", "Culto da Pedra Sombria"),
-                List.of("Mestre da Guilda, Sacerdote misterioso")
-        );
+        return findById(QuestEnum.getRandomName().name());
     }
 
     public Quest findById(String id) {
-        return new Quest(
-                QuestEnum.valueOf(id),
-                "Sussurros nas Minas Antigas " + id,
-                "Mineiros desapareceram, sussurros ecoam nos túneis...",
-                QuestDifficulty.MEDIUM,
-                QuestType.RESCUE,
-                "Minas Ruinosas",
-                "Mineiros relatam sussurros vindos do subsolo.",
-                "Investigar os túneis e descobrir a origem.",
-                "A criatura é manipulada por um culto local.",
-                List.of(GearEnum.GOLD.toItem(50)),
-                "Subterrâneo",
-                List.of(MonsterNameEnum.DARKMANTLE, MonsterNameEnum.GRIMLOCK, MonsterNameEnum.CULT_FANATIC),
-                List.of("Guilda dos Mineradores", "Culto da Pedra Sombria"),
-                List.of("Mestre da Guilda, Sacerdote misterioso")
+        QuestEnum questEnum = QuestEnum.valueOf(id);
+        String enemies = getMessage( id, "enemies");
+        String rewards = getMessage( id, "rewards");
+        String factions = getMessage( id, "factions");
+        String npcs = getMessage( id, "npcs");
+        List<String> factionList = splitList(factions);
+        List<String> npcList = splitList(npcs);
+
+        List<Item> rewardList = new ArrayList<>();
+        Map<String, Integer> stringIntegerMap = parseRewards(rewards);
+        for (Map.Entry<String, Integer> entry : stringIntegerMap.entrySet()) {
+            rewardList.add(GearEnum.valueOf(entry.getKey()).toItem(entry.getValue()));
+        }
+
+        List<MonsterNameEnum> monsterList = !enemies.isEmpty() ?
+                Stream.of(enemies.split(","))
+                    .map(String::trim)
+                    .map(MonsterNameEnum::valueOf)
+                    .toList()
+                : List.of();
+
+        return new Quest(questEnum,
+                         questEnum.getDifficulty(),
+                         questEnum.getType(),
+                         rewardList,
+                         monsterList,
+                         factionList,
+                         npcList
         );
+    }
+
+    public static Map<String, Integer> parseRewards(String rewardStr) {
+        Map<String, Integer> rewards = new HashMap<>();
+        for (String part : rewardStr.split(",")) {
+            part = part.trim();
+            if (part.matches("([A-Z_]+)\\[(\\d+)]")) {
+                String name = part.replaceAll("\\[(\\d+)]", "");
+                int qty = Integer.parseInt(part.replaceAll(".*\\[(\\d+)]", "$1"));
+                rewards.put(name, qty);
+            }
+        }
+        return rewards;
+    }
+
+    private static List<String> splitList(String factions) {
+        return !factions.isEmpty() ? Stream.of(factions.split(",")).map(String::trim).toList() : List.of();
+    }
+
+    private String getMessage(String id, String suffix) {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("tavern.quest." + id + "." + suffix, null, locale);
     }
 }
